e224ff209b8179f3960e503bf5f599ca279db94e
CristianGC <cristian.ghinea.1996@gmail.com>
2023-01-01 14:05:54 +0200

    Create Post Model and Create PostController

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public static function getPosts()
+    {
+        $posts = Post::all();
+
+        return response()->json([
+            'posts' => $posts,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('car','CarController@index');
+Route::apiResource('posts', PostController::class);
 
 /*
 Route::get('posts', 'PostController@index');
